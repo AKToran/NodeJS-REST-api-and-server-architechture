b31db7b34fda1239c7f1b9cdf281aba7d7898764
AKToran <aktoran60@gmail.com>
2026-06-22 16:58:54 +0600

    route handling with HTTP req and res

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,27 @@
 import { createServer, IncomingMessage, Server } from "http";
 
 const server : Server = createServer((req : IncomingMessage, res)=>{
-  console.log(req.url);
-  console.log(req.method); 
+  // console.log(req.url);
+  // console.log(req.method); 
+
+  const url = req.url;
+  const method = req.method;
+
+  if(url === "/" && method === "GET"){
+    // res.writeHead(200, {"content-type" : "text/plain"});
+    // res.end("This is root route.")
+
+    res.writeHead(200, {"content-type" : "application/json"})
+    res.end(JSON.stringify({ message : "This is root route."}));
+  }
+  else if(url?.startsWith("/poducts")){
+    res.writeHead(200, {"content-type" : "application/json"})
+    res.end(JSON.stringify({ message : "This is product route."}));
+  }
+  else{
+    res.writeHead(404, {"content-type" : "application/json"})
+    res.end(JSON.stringify({ message : "Page Not Found!"}));
+  }
   
 })
 
