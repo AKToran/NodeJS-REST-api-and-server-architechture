ac971625d3b71a28ee77f8d3eb0098126660f352
AKToran <aktoran60@gmail.com>
2026-06-24 11:10:54 +0600

    reading file with fs module

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,11 +1,22 @@
 import type { IncomingMessage, ServerResponse } from "http";
+import { readProduct } from "../service/product.service";
 
-export const productController = (req: IncomingMessage, res: ServerResponse) =>{
+export const productController = (
+  req: IncomingMessage,
+  res: ServerResponse,
+) => {
   const url = req.url;
   const method = req.method;
 
-  if(url === "/products" && method === "GET"){
-    res.writeHead(200, {"content-type" : "application/json"})
-    res.end(JSON.stringify({ message : "This is products route."}));
+  const products = readProduct();
+
+  if (url === "/products" && method === "GET") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "This is products route.",
+        data: products,
+      }),
+    );
   }
-}
+};
