5f6866109bccebee40e6c72381144c2c9e2c0bf7
AKToran <aktoran60@gmail.com>
2026-06-24 14:12:23 +0600

    req body parser, confusing

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,8 +1,9 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
 import type { IProduct } from "../types/product.type";
+import { parseBody } from "../utility/parseBody";
 
-export const productController = (
+export const productController = async (
   req: IncomingMessage,
   res: ServerResponse,
 ) => {
@@ -11,10 +12,9 @@ export const productController = (
 
   const urlParts = url?.split('/');
   const id = urlParts && urlParts[1] === 'products'? Number(urlParts[2]): null;
-  console.log(id);
 
   //GET all products  
-  if (url === "/products" && method === "GET") {
+  if ((url === "/products" || url === "/products/") && method === "GET") {
     const products = readProduct();
 
     res.writeHead(200, { "content-type": "application/json" });
@@ -34,6 +34,17 @@ export const productController = (
       JSON.stringify({
         message: "Product retrieved successfully.",
         data: product,
+      }),
+    );
+  }
+  else if( method === "POST" && url === '/products'){
+    const body = await parseBody(req);
+    console.log(body);
+
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Product created successfully.",
       }),
     );
   }
